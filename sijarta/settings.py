5e3dc35fdfeba48982429d02d2db3d48f4fcf3ac
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2024-11-28 18:46:30 +0700

    DEPLOYED DATABASE TO SUPABASE YAYAYAYYA

--- sijarta/settings.py
+++ sijarta/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'sijarta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  
+        'USER': 'postgres.kpmdskkjlxmxitnjoirr',  
+        'PASSWORD': 'BelajarBasdat',  
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',  
+        'PORT': '6543',  
     }
 }
 
