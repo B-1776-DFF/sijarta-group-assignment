9bf88c555affdd7e553b5ff6e7814def2e979653
muzkii <andriyo.averill@ui.ac.id>
2024-12-04 21:25:40 +0700

    Averill, implement register biar konek database, ganti urls sama views sesuai implement

--- aaf/urls.py
+++ aaf/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_user_view, name='register'),
+    path('register/', views.register_view, name='register'),
     path('register/user/', views.register_user_view, name='register_user'),
     path('register/worker/', views.register_worker_view, name='register_worker'),
     path('profile/', views.profile_view, name='profile'),
