179f789bbf71c8434927b5fc8c01e2fc8af76d71
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-12-07 20:50:34 +0700

    ijo -worker page

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('subcategory/<int:subcategory_id>/user/', views.subcategory_user_view, name='subcategory_user'),
-    path('subcategory/<int:subcategory_id>/worker/', views.subcategory_worker_view, name='subcategory_worker'),
-    path('subcategory/<int:subcategory_id>/join/', views.join_subcategory, name='join_subcategory'),
-    path('book-session/<int:session_id>/', views.book_session, name='book_session'),
-    path('subcategory/<int:subcategory_id>/workers/json', views.subcategory_worker_view_json, name='subcategory_worker_view_json'),
+    path('subcategory/<uuid:subcategory_id>/user/', views.subcategory_user, name='subcategory_user'),
+    path('subcategory/<uuid:subcategory_id>/worker/', views.subcategory_worker, name='subcategory_worker'),
+    # path('subcategory/<int:subcategory_id>/join/', views.join_subcategory, name='join_subcategory'),
+    # path('book-session/<int:session_id>/', views.book_session, name='book_session'),
 
 ]
